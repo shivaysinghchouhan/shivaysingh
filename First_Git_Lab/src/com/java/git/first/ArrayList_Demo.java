c563bc789555102b578d3a297c43e4aef1696b13
shivaysinghchouhan <shivrajchouhan64@gmail.com>
2022-11-16 03:35:38 -0800

    add some another file

---
+++ First_Git_Lab/src/com/java/git/first/ArrayList_Demo.java
@@ -0,0 +1,16 @@
+package com.java.git.first;
+
+import java.util.ArrayList;
+
+public class ArrayList_Demo {
+public static void main(String[] args) {
+	ArrayList arrayList = new ArrayList ();
+	arrayList.add("jasraj");
+	arrayList.add("indore");
+	arrayList.add("velocity");
+	
+	System.out.println(arrayList);
+	
+	
+}
+}
---
+++ First_Git_Lab/src/com/java/git/first/Set_Demo.java
@@ -0,0 +1,21 @@
+package com.java.git.first;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Set_Demo {
+
+	public static void main(String[] args) {
+		Set set = new HashSet();
+		set.add("jasraj");
+		set.add("piyush");
+		set.add("yash ");
+		set.add("jyoti");
+		set.add("jasraj");
+		set.add("piyush");
+		set.add("yash ");
+		set.add("jyoti");
+		System.out.println(set);
+	}
+
+}
